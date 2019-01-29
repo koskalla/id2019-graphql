c3be5d5cbfe18fa525a33126353738b7968e39ff
Kimmo Koskela Ståhl <kimmo_koskela@hotmail.com>
2019-01-29 13:21:30 +0100

    rest controllers added

---
+++ NobelPriceRestAPI/Controllers/NobelPricesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NobelPriceData.Models;
+
+namespace NobelPriceRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NobelPricesController : ControllerBase
+    {
+
+        private readonly NobelDataContext _db;
+
+        public NobelPricesController(NobelDataContext db)
+        {
+            _db = db;
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public async Task<List<NobelPrizes>> Get()
+        {
+            return await _db.NobelPrizes.ToListAsync();
+        }
+
+        // GET: api/NobelPrices/5
+        [HttpGet("{year}", Name = "GetPricesByYear")]
+        public async Task<List<NobelPrizes>> Get(string year)
+        {
+            return await _db.NobelPrizes.Where(x => x.Year.ToString() == year).ToListAsync();
+        }
+
+        // POST: api/NobelPrices
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/NobelPrices/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NobelPriceRestAPI/Controllers/NobelWinnersController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NobelPriceData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NobelPriceRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NobelWinnersController : ControllerBase
+    {
+        private readonly NobelDataContext _db;
+
+        public NobelWinnersController(NobelDataContext db)
+        {
+            _db = db;
+        }
+
+        // GET: api/NobelWinners
+        [HttpGet]
+        public async Task<List<NobelWinners>> Get()
+        {
+            return await _db.NobelWinners.ToListAsync();
+
+            //return new string[] { "value1", "value2" };
+        }
+
+        //// GET: api/NobelWinners/5
+        [HttpGet("{name}", Name = "GetWinnersByName")]
+        public async Task<List<NobelWinners>> Get(string name)
+        {
+            return await _db.NobelWinners.Where(x => x.Surname == name).ToListAsync();
+        }
+
+        // POST: api/NobelWinners
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/NobelWinners/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
